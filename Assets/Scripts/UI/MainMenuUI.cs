7c733695268156cccdf6f9c8fe39f1a0b3b85f31
Sultan Galiullin <sultangal@outlook.com>
2023-05-26 17:53:05 +0300

    Refactored code. Fixed mistakes.

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,11 +11,21 @@ public class MainMenuUI : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
+        GameManager.Instance.OnGameStateChanged += GameManager_OnGameStateChanged;
+
         btnPlay.GetComponent<Button>().onClick.AddListener(() =>
         {
             GameManager.Instance.SetGameState(GameManager.GameState.GamePlaying);
             btnPlay.gameObject.SetActive(false);
         });
 
+    }
+
+    private void GameManager_OnGameStateChanged(object sender, System.EventArgs e)
+    {
+        if (GameManager.Instance.IsGameEnd())
+        {
+            btnPlay.gameObject.SetActive(true);
+        }
     }
 }
