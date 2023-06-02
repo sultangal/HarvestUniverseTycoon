58e72dde51f3f4d91ed18d3c431b6e3c0dfc7c34
Sultan Galiullin <sultangal@outlook.com>
2023-06-02 16:40:27 +0300

    Added acceleration and deceleration of vehicle. Refactored MoveVehicle() method.

--- Assets/Scripts/VehicleControl.cs
+++ Assets/Scripts/VehicleControl.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class VehicleControl : MonoBehaviour
 {
 
-    [SerializeField] private float moveSpeed = 10f;  // Rotation speed in degrees per second
+    [SerializeField] private float maxSpeed = 50f;  // Rotation speed in degrees per second
     [SerializeField] private Transform vehicleRef;
     [SerializeField] Vector3 halfExtends;
     private Vector2 inputDirection;
     private Vector3 vehicleAngles;
     [SerializeField] private FloatingJoystick joystick;
 
+    private float timeZeroToMax = 0.5f;
+    private float timeMaxToZero = 0.5f;
+    private float accelRatePerSec;
+    private float decelRatePerSec;
+    private float forwardVelocity = 0.0f;
+
     private void Start()
     {
+        accelRatePerSec = maxSpeed / timeZeroToMax;
+        decelRatePerSec = -maxSpeed / timeMaxToZero;
         GameManager.Instance.OnGameStateChanged += GameManager_OnGameStateChanged;
     }
     private void Update()
@@ -21,29 +29,46 @@ public class VehicleControl : MonoBehaviour
         if (GameManager.Instance.IsGamePlaying())
         {
             //Debug.Log("Input" + joystick.Horizontal);
-            inputDirection.y = joystick.Horizontal;
-            inputDirection.x = joystick.Vertical;
-            inputDirection.Normalize();
+            //inputDirection.y = joystick.Horizontal;
+            //inputDirection.x = joystick.Vertical;
+            //inputDirection.Normalize();
+
+            //Debug.Log("joystick.Horizontal " + joystick.Horizontal);
+            //Debug.Log("joystick.Vertical " + joystick.Vertical);            
+            //Debug.Log("inputDirection " + inputDirection);
             MoveVehicle();
         }
     }
 
     private void MoveVehicle()
     {
-        Quaternion rotation = transform.rotation;
 
-        // Rotate around y-axis with left/right arrow keys
-        Quaternion horizontalRotation = Quaternion.Euler(0f, 0f, -(inputDirection.y * moveSpeed * Time.deltaTime));
-        rotation *= horizontalRotation;
+        if (joystick.Horizontal != 0.0f && joystick.Vertical != 0.0f)
+        {
+            //calculation of acceleration
+            forwardVelocity += accelRatePerSec * Time.deltaTime;
+            forwardVelocity = Mathf.Min(forwardVelocity, maxSpeed);
 
-        // Rotate around x-axis with up/down arrow keys
-        Quaternion verticalRotation = Quaternion.Euler(inputDirection.x * moveSpeed * Time.deltaTime, 0f, 0f);
-        rotation *= verticalRotation;
+            //saving last entered direction
+            inputDirection.y = joystick.Horizontal;
+            inputDirection.x = joystick.Vertical;
+            //normalizing to avoid when vehicle move faster in diagonal directions
+            inputDirection.Normalize();
+        }
+        else
+        {
+            //calculation of deceleration
+            forwardVelocity += decelRatePerSec * Time.deltaTime;
+            forwardVelocity = Mathf.Max(forwardVelocity, 0);
+        }
 
-        // Apply the final rotation
-        transform.rotation = rotation;
+        transform.rotation *= Quaternion.Euler(
+            inputDirection.x * forwardVelocity * Time.deltaTime,
+            0f,
+            -(inputDirection.y * forwardVelocity * Time.deltaTime));
 
         Direction();
+
     }
 
     private void Direction()
@@ -55,7 +80,6 @@ public class VehicleControl : MonoBehaviour
             vehicleAngles.z = vehicleRef.transform.localEulerAngles.z;
 
             vehicleRef.transform.localEulerAngles = vehicleAngles;
-
         }
     }
 
