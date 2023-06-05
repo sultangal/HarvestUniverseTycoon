881b2b777e03b473b065fe2a0454cc436dfc7c83
Sultan Galiullin <sultangal@outlook.com>
2023-06-05 15:41:18 +0300

    Added camera movement.

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -1,11 +1,22 @@
+using System.Collections;
 using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
+    [SerializeField] private float timeToAjust = 2.0f;
+    private float accelRatePerSec;
+    private float decelRatePerSec;
+    private float horizVelocity = 0.0f;
+    private float vertVelocity = 0.0f;
+
     [SerializeField] private Camera cameraRef;
+    [SerializeField] private FloatingJoystick joystick;
+    [SerializeField] AnimationCurve cameraCurve;
     private const float MIN_ASPECT_RATIO = 0.5625f;
     private void Start()
     {
+        accelRatePerSec = 1f / timeToAjust;
+        decelRatePerSec = -1f / timeToAjust;
         //cameraRef = GetComponent<Camera>();
 #if !UNITY_EDITOR
         float aspectRatio = (float)Screen.currentResolution.width / (float)Screen.currentResolution.height;
@@ -21,6 +32,8 @@ public class CameraControl : MonoBehaviour
         {
             cam.fieldOfView = 55.5f;
         }
+
+        CameraMotion();
 #endif
     }
 
@@ -41,18 +54,49 @@ public class CameraControl : MonoBehaviour
         {
             cameraRef.fieldOfView = 55.5f;
         }
+
+        CameraMotion();
     }
 
     public static Vector2 GetMainGameViewSize()
     {
         System.Type T = System.Type.GetType("UnityEditor.GameView,UnityEditor");
-        System.Reflection.MethodInfo GetSizeOfMainGameView = T.GetMethod("GetSizeOfMainGameView", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+        System.Reflection.MethodInfo GetSizeOfMainGameView = T.GetMethod("GetSizeOfMainGameView", 
+            System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
         System.Object Res = GetSizeOfMainGameView.Invoke(null, null);
         return (Vector2)Res;
     }   
 #endif
 
 
+    private void CameraMotion()
+    {
 
+        if (joystick.Vertical > 0f)
+        {
+            horizVelocity += accelRatePerSec * Time.deltaTime;
+            horizVelocity = Mathf.Min(horizVelocity, 1f);         
+        }
+        else if (joystick.Vertical < 0f)
+        {
+            horizVelocity += decelRatePerSec * Time.deltaTime;
+            horizVelocity = Mathf.Max(horizVelocity, 0f);
+        }
+
+        if (joystick.Horizontal < 0f)
+        {
+            vertVelocity += accelRatePerSec * Time.deltaTime;
+            vertVelocity = Mathf.Min(vertVelocity, 1f);
+        }
+        else if (joystick.Horizontal > 0f)
+        {
+            vertVelocity += decelRatePerSec * Time.deltaTime;
+            vertVelocity = Mathf.Max(vertVelocity, 0f);
+        }
+
+        transform.localEulerAngles = new(cameraCurve.Evaluate(horizVelocity), 0f, cameraCurve.Evaluate(vertVelocity));
+
+        // Debug.Log("cameraCurve.Evaluate(forwardHorizVelocity) " + cameraCurve.Evaluate(forwardHorizVelocity));
+    }
 
 }
