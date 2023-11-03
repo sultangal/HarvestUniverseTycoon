48a1424e6d65aaac0466969fabea797d709a59e7
Sultan Galiullin <sultangal@outlook.com>
2023-11-04 00:23:31 +0300

    Skybox rotation now uses DOTween.

--- Assets/Scripts/Skybox.cs
+++ Assets/Scripts/Skybox.cs
@@ -1,77 +1,38 @@
-using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 
 public class Skybox : MonoBehaviour
 {
     [SerializeField] AnimationCurve animCurve;
     private Material skyboxMat;
-    private float skyboxRotationCurr;
-    private float skyboxRotationGoal;
-    private readonly float ROTATION_AMOUNT = 1f;
-    private bool rotateRightFlag;
-    private bool rotateLeftFlag;
-    private float time;
+    private readonly float ROTATION_MULT = 5f;
+    private readonly float ANIM_DURATION = 1f;
+
     private void Start()
     {
         Planets.Instance.OnPlanetShift += PlanetsController_OnPlanetShift;
         skyboxMat = RenderSettings.skybox;
-        skyboxRotationCurr = 0f;
-        rotateRightFlag = false;
-        rotateLeftFlag = false;
-        time = 0f;
-
+        skyboxMat.SetFloat("_Rotation", 0f);
     }
-
 
     private void PlanetsController_OnPlanetShift(object sender, Planets.OnPlanetShiftEventArgs e)
     {
-        if (e.isRight)
-        {
-            skyboxRotationGoal = skyboxRotationCurr + ROTATION_AMOUNT;
-            //var keys = animCurve.keys;
-            //keys[0].value = skyboxRotationCurr;
-            //keys[1].value = skyboxRotationGoal;
-            rotateRightFlag = true;           
-        } else
-        {
-            skyboxRotationGoal = skyboxRotationCurr - ROTATION_AMOUNT;
-            rotateLeftFlag = true;            
-        }
-
+        if (e.isRight)                
+            DOTween.To(
+                () => skyboxMat.GetFloat("_Rotation"),
+                x => skyboxMat.SetFloat("_Rotation", x),
+                skyboxMat.GetFloat("_Rotation") + ROTATION_MULT,
+                ANIM_DURATION);              
+        else
+            DOTween.To(
+                () => skyboxMat.GetFloat("_Rotation"),
+                x => skyboxMat.SetFloat("_Rotation", x),
+                skyboxMat.GetFloat("_Rotation") - ROTATION_MULT,
+                ANIM_DURATION);
     }
 
-    private void Update()
+    private void OnDestroy()
     {
-        if (rotateRightFlag)
-        {
-            RotateSkyboxRight();
-        }
-        else if (rotateLeftFlag) 
-        {
-            RotateSkyboxLeft();
-        }
-    }
-
-    private void RotateSkyboxRight()
-    {
-        //time += Time.deltaTime;
-        //skyboxRotationCurr = animCurve.Evaluate(time);
-        skyboxRotationCurr += Time.deltaTime;
-        skyboxMat.SetFloat("_Rotation", skyboxRotationCurr * 3);
-        if (skyboxRotationCurr > skyboxRotationGoal) {
-            time = 0f;
-            rotateRightFlag = false;
-        }
-    }
-
-    private void RotateSkyboxLeft()
-    {
-        skyboxRotationCurr -= Time.deltaTime;
-        skyboxMat.SetFloat("_Rotation", skyboxRotationCurr * 3f);
-        if (skyboxRotationCurr < skyboxRotationGoal)
-        {
-            time = 0f;
-            rotateLeftFlag = false;
-        }
+        skyboxMat.SetFloat("_Rotation", 0f);
     }
 }
