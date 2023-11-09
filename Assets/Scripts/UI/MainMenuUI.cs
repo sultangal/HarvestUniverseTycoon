d0a2ff0c1c9691d93e8b69fa429627d517cfe27d
Sultan Galiullin <sultangal@outlook.com>
2023-11-09 16:14:30 +0300

    Gold and cash now appears in the main menu.

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -9,6 +9,8 @@ public class MainMenuUI : MonoBehaviour
     [SerializeField] private Button btnShiftRight;
     [SerializeField] private Transform textsGroup;
     [SerializeField] private TextMeshProUGUI level;
+    [SerializeField] private TextMeshProUGUI gold;
+    [SerializeField] private TextMeshProUGUI cash;
 
     private void Start()
     {
@@ -45,6 +47,8 @@ public class MainMenuUI : MonoBehaviour
         if (GameManager.Instance.IsGameWaitingToStart())
         {
             level.text = GameManager.Instance.GlobalData_.level.ToString();
+            gold.text = GameManager.Instance.GlobalData_.amountOfGold.ToString();
+            cash.text = "$ " + GameManager.Instance.GlobalData_.amountOfCash.ToString();
             btnPlay.gameObject.SetActive(true);
             btnShiftLeft.gameObject.SetActive(true);
             btnShiftRight.gameObject.SetActive(true);
