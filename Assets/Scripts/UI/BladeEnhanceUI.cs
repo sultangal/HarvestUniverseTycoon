97a2f0734044abee3bfbdf335b8d9420a18fcf7a
Sultan Galiullin <sultangal@outlook.com>
2023-11-13 12:26:51 +0300

    Visibility logic was added to blades enhance. Code was refactored.

--- Assets/Scripts/UI/BladeEnhanceUI.cs
+++ Assets/Scripts/UI/BladeEnhanceUI.cs
@@ -4,28 +4,55 @@ using UnityEngine.UI;
 
 public class BladeEnhanceUI : MonoBehaviour
 {
-    public Button button;
-    public TextMeshProUGUI cost;
-    public UnityEngine.UI.Image image;
+    [SerializeField] private GameObject bladeGroup;
+    [SerializeField] private Button button;
+    [SerializeField] private TextMeshProUGUI cost;
+    [SerializeField] private Image image;
 
     private void Start()
     {
         button.onClick.AddListener(() =>
         {
-            if (!HarvesterGroup.Instance.TryEnhanceBlades())
-            {
-                Debug.Log("You broke hahahaah!!");
-            }
+            HarvesterGroup.Instance.TryEnhanceBlades();
+            UpdateBladeEnhanceAvailability();
         });
 
         GameManager.Instance.OnGameStateChanged += GameManager_OnGameStateChanged;
+        UpdateBladeEnhanceAvailability();
     }
 
     private void GameManager_OnGameStateChanged(object sender, System.EventArgs e)
     {
         if (GameManager.Instance.IsGameWaitingToStart())
         {
+            bladeGroup.SetActive(true);
+            UpdateBladeEnhanceAvailability();
             cost.text = Planets.Instance.GetBladesEnhanceCost().ToString();
+        } else
+            bladeGroup.SetActive(false);
+    }
+
+    private void UpdateBladeEnhanceAvailability()
+    {
+        if (GameManager.Instance.GlobalData_.amountOfCash >= Planets.Instance.GetBladesEnhanceCost())
+            SetButtonInteractivity(true);
+        else
+            SetButtonInteractivity(false);
+    }
+
+    private void SetButtonInteractivity(bool isInteractable)
+    {
+        if (isInteractable)
+        {
+            button.interactable = true;
+            image.color = Color.white;
+            cost.color = Color.green;
+        }
+        else
+        {
+            button.interactable = false;
+            image.color = Color.gray;
+            cost.color = Color.gray;
         }
     }
 }
