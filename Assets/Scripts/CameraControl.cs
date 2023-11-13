da0a951232f9d9d1ccbdcf2e061682df6986c0d2
Sultan Galiullin <sultangal@outlook.com>
2023-11-13 18:41:45 +0300

    Bug with CameraControl was fixed.

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -40,7 +40,17 @@ public class CameraControl : MonoBehaviour
         {
             CameraMotion();
         }
+    } 
+
+    public static Vector2 GetMainGameViewSize()
+    {
+        System.Type T = System.Type.GetType("UnityEditor.GameView,UnityEditor");
+        System.Reflection.MethodInfo GetSizeOfMainGameView = T.GetMethod("GetSizeOfMainGameView", 
+            System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+        System.Object Res = GetSizeOfMainGameView.Invoke(null, null);
+        return (Vector2)Res;
     }
+#endif
 
     private void CorrectCameraFOV(float aspectRatio)
     {
@@ -74,17 +84,6 @@ public class CameraControl : MonoBehaviour
             else cameraRef.transform.DORotate(CAMERA_MENU_ROTATION, 1f);
         }
     }
-
-    public static Vector2 GetMainGameViewSize()
-    {
-        System.Type T = System.Type.GetType("UnityEditor.GameView,UnityEditor");
-        System.Reflection.MethodInfo GetSizeOfMainGameView = T.GetMethod("GetSizeOfMainGameView", 
-            System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
-        System.Object Res = GetSizeOfMainGameView.Invoke(null, null);
-        return (Vector2)Res;
-    }   
-#endif
-
 
     private void CameraMotion()
     {
